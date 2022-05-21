45feb64da1c9abe536aa4c47f4fbd703851203d6
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-05-21 12:13:42 +0100

    esquema de habitación implementado

--- habitaciones/models.py
+++ habitaciones/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class habitaciones(models.Model):
+    nombre_de_la_habitación = models.CharField("max_length=50")
+    capacidad = models.IntegerField()
+
