e09e30e2676980307b291c017aa998aec0d95c69
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-05-21 15:14:26 +0100

    campos agregados al modelo de habitaciones y al modelo de cuotas

--- cuotas/models.py
+++ cuotas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class cuotas(models.Model):
-    numbre_de_la_cuotas = models.CharField("numbre_de_la_cuotas", max_length=50)
+    numbre_de_la_cuotas = models.CharField("numbre_de_la_cuotas", max_length=50, default=None)
     Tipo_de_tarifa = models.CharField("Tipo_de_tarifa", max_length=50)
     numbre_de_la_tarifa = models.CharField("Tipo_de_tarifa", max_length=50)
     actividad = models.ForeignKey("clases.Clases", verbose_name="actividades para agregar para agregar", on_delete=models.CASCADE, blank=True)
@@ -10,5 +10,4 @@ class cuotas(models.Model):
 
     def __str__(self) -> str:
         return self.numbre_de_la_cuotas
-    
     
--- habitaciones/models.py
+++ habitaciones/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class habitaciones(models.Model):
-    nombre_de_la_habitacion = models.CharField(max_length=50)
+    nombre_de_la_habitacion = models.CharField(max_length=50, null=True, blank=True)
     capacidad = models.IntegerField()
 
     def __str__(self) -> str:
