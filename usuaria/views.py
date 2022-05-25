e1c976902405115d8ebc961cfaa2dad51adcef37
thoth2357 <98170427+thoth2357@users.noreply.github.com>
2022-05-25 14:33:30 +0100

    permiso del tablero trabajado en

--- usuaria/views.py
+++ usuaria/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+@login_required
 def dashboard_view(request):
     context = {}
     if request.method == 'GET':
+        print(request.user)
         if request.user.is_staff == True:
             print('A Staff here')
         else:
-            pass
+            print('non staff here')
     return render(request, 'dashboard/dashboard.html', context)
 
 
